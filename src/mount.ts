43ab36917a34c20bceb428c4ce86dc3fa470c4b5
batogov <thesiamesecat19@gmail.com>
2019-01-17 08:25:43 +0300

    Add return value from mount function

--- src/mount.ts
+++ src/mount.ts
@@ -1,10 +1,13 @@
 import { RealDOMElem } from './render';
 
-const mount = ($node: RealDOMElem, $target: RealDOMElem | null) => {
+const mount = ($node: RealDOMElem, $target: RealDOMElem | null): RealDOMElem | null => {
     if ($target !== null) {
         // Ignore Internet Explorer and Safari
         $target.replaceWith($node);
+        return $node;
     }
+
+    return null;
 };
 
 export default mount;
