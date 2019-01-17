7a91a639e8e2c17be0210c411850f237e025a520
batogov <thesiamesecat19@gmail.com>
2019-01-17 08:27:14 +0300

    Add live counter to index file

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,15 @@ import createElement from './createElement';
 import render from './render';
 import mount from './mount';
 
-const vApp = createElement('div', {
+
+const createVirtualApp = (count: number) => createElement('div', {
     attrs: {
         id: 'app',
+        "data-count": count,
     },
     children: [
-        // TextNode
-        'Hello, world!',
-        // ElementNode
+        'The current count is: ',
+        String(count),
         createElement('img', {
             attrs: {
                 src: 'https://media.giphy.com/media/129NVCr1UfsGTS/giphy.gif',
@@ -18,5 +19,14 @@ const vApp = createElement('div', {
     ],
 });
 
+let count = 0;
+
+const vApp = createVirtualApp(count);
 const $app = render(vApp);
-mount($app, document.getElementById('app'));
+
+let $root = mount($app, document.getElementById('app'));
+
+setInterval(() => {
+    count++;
+    $root = mount(render(createVirtualApp(count)), $root);
+}, 1000);
